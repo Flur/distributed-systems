3147a0468bb9f070ba605cdb0ca84e6d75221d8c
Diana Dmytrashko <d.dmytrashko@autodoc.eu>
2021-01-04 22:33:24 +0200

    add add-message-list

--- secondary/secondary.py
+++ secondary/secondary.py
@@ -14,14 +14,16 @@ message_list = []
 message_ids = []
 
 @app.route('/')
-def hello_world(message_list = message_list):
-    for i in range(1, len(message_list)):
-        if (message_list[i-1]["id"] + 1) != message_list[i]["id"]:
-            message_list = message_list[:i]
-    return json.dumps(message_list)
+def hello_world():
+    new_message_list = message_list.copy()
+    for i in range(1, len(new_message_list)):
+        if (new_message_list[i-1]["id"] + 1) != new_message_list[i]["id"]:
+            new_message_list = new_message_list[:i]
+    return json.dumps(new_message_list)
 
 class SocketServer:
     HOST = '127.0.0.1'
+    print(sys.argv)
     PORT = int(sys.argv[3]) + 1 # variant of solution https://github.com/pallets/flask/issues/3095
     def __init__(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -53,6 +55,11 @@ class SocketServer:
         if event["eventType"] == "add-message" and event["message"]["id"] not in message_ids:
             message_list.append(event["message"])
             message_ids.append(event["message"]["id"])
+        elif event["eventType"] == "add-messages-list":
+            for i in event["message"]:
+                if i["id"] not in message_ids:
+                    message_list.append(i)
+                    message_ids.append(i["id"])
         event_message = {
             "eventType": "ok"
         }
